e4c951bd9df9293bb281ddea23d40cfdf4d77141
dust <ypjch521>
2017-07-29 18:11:32 +0800

    1.groovy的相关代码    练习 2.

---
+++ app/src/main/java/com/example/dust/persondemo/factory/Test1.java
@@ -0,0 +1,29 @@
+package com.example.dust.persondemo.factory;
+
+class Test1{
+
+
+ public static void main(String[] args) {
+
+
+  System.out.println ("第五题  ----------------------------");
+
+    //我开始觉得 应该是 97  和 98   结果呢  a  a1
+
+    System.out.println('a');
+    System.out.println('a'+1);
+
+     System.out.println ( "第六题  ----------------------------");
+
+    System.out. println("hello"+'a'+1);
+    System.out.println('a'+1+"hello");
+
+ 	
+	
+}
+
+
+
+
+
+}
