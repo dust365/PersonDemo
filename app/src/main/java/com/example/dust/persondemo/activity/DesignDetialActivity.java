823f21573d6e31a27fdf1562f636dec6dec8f58d
huichen <103123485@qq.com>
2018-05-21 11:11:49 +0800

    1.自定义  Behavior  实现各种联动效果

--- app/src/main/java/com/example/dust/persondemo/activity/DesignDetialActivity.java
+++ app/src/main/java/com/example/dust/persondemo/activity/DesignDetialActivity.java
@@ -51,51 +51,6 @@ public class DesignDetialActivity extends AppCompatActivity {
     private void testRxjava() {
 
 
-
-//        //创建一个上游 Observable：
-//        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
-//            @Override
-//            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-//                emitter.onNext(1);
-//                emitter.onNext(2);
-//                emitter.onNext(3);
-//                emitter.onComplete();
-//
-//                emitter.onNext(43);
-////                Throwable throwable = new Throwable("错误");
-////                emitter.onError(throwable);
-//
-//                /**
-//                 *   onComplete     onError  二者互斥不能同时存在
-//                 */
-//
-//            }
-//        });
-//        //创建一个下游 Observer
-//        Observer<Integer> observer = new Observer<Integer>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                Log.d(TAG, "subscribe");
-//            }
-//
-//            @Override
-//            public void onNext(Integer value) {
-//                Log.d(TAG, "" + value);
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.d(TAG, "error");
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d(TAG, "complete");
-//            }
-//        };
-//        //建立连接
-//        observable.subscribe(observer);
-
   Observable.create(new ObservableOnSubscribe<Integer>() {
       @Override
       public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
---
+++ app/src/main/java/com/example/dust/persondemo/behavior/MyBehavior.java
@@ -0,0 +1,62 @@
+package com.example.dust.persondemo.behavior;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * @author dust
+ * @function   自定义    MyBehavior
+ * @created at 2018/5/21
+ */
+public class MyBehavior  extends CoordinatorLayout.Behavior {
+
+
+    /**
+     * 如果布局使用这个     必须重写这个两参数构造
+     * @param context
+     * @param attrs
+     */
+    public MyBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+//        return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+        return true;
+
+
+
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+
+
+        if (dy<0){
+
+
+            ViewCompat.animate(child).scaleX(1).scaleY(1).start();
+
+        }else {
+
+            ViewCompat.animate(child).scaleX(0).scaleY(0).start();
+
+        }
+    }
+
+    @Override
+    public boolean onNestedPreFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, float velocityX, float velocityY) {
+        return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
+
+
+
+
+    }
+}
